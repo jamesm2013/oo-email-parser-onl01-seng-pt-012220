a64123117bf3707f12313d79c1c595b9db2f73ac
james Johnson <2013matthew2013@gmail.com>
2020-02-15 14:51:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,2 +1,14 @@
+class EmailAddressParser
 
+  attr_accessor :emails
+
+  def initialize(emails)
+    self.emails = emails
+  end
+
+  def parse
+    self.emails.split(/ |\, /).each {|x| x.strip}.uniq
+  end
+
+end 
 
